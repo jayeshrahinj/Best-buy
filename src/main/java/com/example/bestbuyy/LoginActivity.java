10ea1c408d126897f72b8b3bbb581f4f533656fc
Jayesh Rahinj <103485215+jayeshrahinj@users.noreply.github.com>
2023-02-22 13:54:12 +0530

    Add files via upload

---
+++ src/main/java/com/example/bestbuyy/LoginActivity.java
@@ -0,0 +1,139 @@
+package com.example.bestbuyy;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import android.widget.Toast;
+
+import com.example.bestbuyy.databinding.ActivityLoginBinding;
+//import com.google.android.gms.auth.api.signin.GoogleSignIn;
+//import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+//import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+//import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+//import com.google.android.gms.common.api.ApiException;
+//import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity {
+//   private ImageView btngoogle;
+//   public GoogleSignInClient client;
+    ActivityLoginBinding binding;
+    FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        btngoogle.findViewById(R.id.btngoogle);
+        firebaseAuth=FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.email.getText().toString().trim();
+                String password = binding.password.getText().toString().trim();
+                progressDialog.show();
+                firebaseAuth.signInWithEmailAndPassword(email,password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                progressDialog.cancel();
+                                startActivity(new Intent(LoginActivity.this,home_Navbar_Activity.class));
+
+                                Toast.makeText(LoginActivity.this, "Login succesfull", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                progressDialog.cancel();
+                                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+            }
+        });
+        binding.resetpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.email.getText().toString();
+                progressDialog.show();
+                progressDialog.setTitle("Sending mail");
+                firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                progressDialog.cancel();
+
+                                Toast.makeText(LoginActivity.this, "Email send", Toast.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                progressDialog.cancel();
+                                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+        binding.gotoSignupActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
+        });
+
+
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode==1234)
+//        {
+//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+//            try {
+//                GoogleSignInAccount account = task.getResult(ApiException.class);
+//                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+//                FirebaseAuth.getInstance().signInWithCredential(credential)
+//                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                if (task.isSuccessful())
+//                                {
+//                                 Intent intent = new Intent(getApplicationContext(),googlesignActivity.class);
+//                                 startActivity(intent);
+//                                }else
+//                                {
+//                                    Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                                }
+//                            }
+//                        });
+//            }
+//            catch (ApiException e)
+//            {
+//                e.printStackTrace();
+//            }
+//
+//        }
+//    }
+}
